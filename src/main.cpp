9058358ea239b18ab606070503d44b5379d3e104
idrazzhivin <55536659+idrazzhivin@users.noreply.github.com>
2021-04-27 21:32:29 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,29 @@
 #include "TimedDoor.h"
 
 int main() {
-  TimedDoor timedDoor(5);  // create the door
+  TimedDoor timedDoor(5);  // create the door&tmr
+  TimedDoor* timedDoor1 = new TimedDoor(5);  // same
   try {
     timedDoor.unlock();
   } catch (std::string notice) {
     std::cout << notice << std::endl;  // close
+    timedDoor1->unlock();
+    timedDoor1->throwState();
+  } catch (std::string notice1) {
+    std::cout << notice1 << std::endl;  // open
+  }
+  TimedDoor* timedDoor2 = new TimedDoor(5);
+  try {
+    timedDoor2->unlock();
+  } catch (std::string notice2) {
+    std::cout << notice2 << std::endl;  // close
+  }
+  TimedDoor* timedDoor3 = new TimedDoor(5);
+  try {
+    timedDoor3->lock();
+    timedDoor3->throwState();
+  } catch (std::string notice3) {
+    std::cout << notice3 << std::endl;  // close
   }
   return 0;
 }
